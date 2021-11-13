4f9c66bf228d91e568d817d43db5082a082d1da7
youngkyunIm <yee0230@naver.com>
2021-11-13 16:50:09 +0900

    mapstruct try again

--- booking_service_01/src/main/java/com/example/booking_service_01/mapper/FacilityMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.booking_service_01.mapper;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
-@Mapper
-public interface FacilityMapper{
-    FacilityMapper INSTANCE = Mappers.getMapper(FacilityMapper.class);
-}
--- booking_service_01/src/main/java/com/example/booking_service_01/mapper/ManageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.booking_service_01.mapper;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
-@Mapper
-public interface ManageMapper{
-    ManageMapper INSTANCE = Mappers.getMapper(ManageMapper.class);
-}
