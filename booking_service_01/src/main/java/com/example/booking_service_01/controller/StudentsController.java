27d69f1c91650c8047414a3466c6748457c0d66f
youngkyunIm <yee0230@naver.com>
2021-11-16 22:08:30 +0900

    Students Delete

--- booking_service_01/src/main/java/com/example/booking_service_01/controller/StudentsController.java
+++ booking_service_01/src/main/java/com/example/booking_service_01/controller/StudentsController.java
@@ -1,12 +1,14 @@
 package com.example.booking_service_01.controller;
 
 import com.example.booking_service_01.dto.StudentsDTO;
+import com.example.booking_service_01.entity.Students;
 import com.example.booking_service_01.repository.StudentsRepository;
 import com.example.booking_service_01.service.StudentsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +25,7 @@ public class StudentsController {
     StudentsRepository studentsRepository;
     //Select 
     @GetMapping(path="/{sid}", produces = "application/json")
-    public ResponseEntity<?> getAno(@PathVariable("sid") Integer sid) {
+    public ResponseEntity<?> getSid(@PathVariable("sid") Integer sid) {
         if(!studentsService.checkSid(sid)) {
             return new ResponseEntity<>("sid can not found", HttpStatus.NOT_ACCEPTABLE);
         }
@@ -34,7 +36,7 @@ public class StudentsController {
     }
     //Insert
     @PostMapping(path = "/join", produces = "application/json")
-    public ResponseEntity<?> insertAdmin(@RequestBody StudentsDTO studentsDTO) {
+    public ResponseEntity<?> insertStudent(@RequestBody StudentsDTO studentsDTO) {
         if(studentsDTO.getSid()!=null) {
             if (studentsService.checkSid(studentsDTO.getSid()))
                 return new ResponseEntity<>(HttpStatus.PRECONDITION_FAILED);
@@ -42,6 +44,17 @@ public class StudentsController {
         }
         else{
             return new ResponseEntity<>(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+    //Delete
+    @DeleteMapping(path="/{sid}", produces = "application/json")
+    public ResponseEntity<?> deleteStudent(@PathVariable("sid") Integer sid) {
+        if(!studentsService.checkSid(sid))
+            return new ResponseEntity<>("Admin ID can not found", HttpStatus.NOT_ACCEPTABLE);
+        else {
+            StudentsDTO studentsDTO = studentsService.findBySid(sid);
+            studentsService.delete(studentsDTO);
+            return new ResponseEntity<Void>(HttpStatus.OK);
         }
     }
 }
