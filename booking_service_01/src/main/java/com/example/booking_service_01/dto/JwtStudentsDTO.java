c0a084637be3ccd30c8bcc551dd7579d127c7e69
youngkyunIm <yee0230@naver.com>
2021-11-16 21:59:40 +0900

    sudent_dto

---
+++ booking_service_01/src/main/java/com/example/booking_service_01/dto/JwtStudentsDTO.java
@@ -0,0 +1,9 @@
+package com.example.booking_service_01.dto;
+
+import lombok.Getter;
+
+@Getter
+public class JwtStudentsDTO {
+    private Integer sid;
+    private String pw;
+}
--- booking_service_01/src/main/java/com/example/booking_service_01/dto/StudentsDTO.java
+++ booking_service_01/src/main/java/com/example/booking_service_01/dto/StudentsDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentsDTO {
-    private String sid;
+    private Integer sid;
     private String pw;
     private String name;
     private String phone;
