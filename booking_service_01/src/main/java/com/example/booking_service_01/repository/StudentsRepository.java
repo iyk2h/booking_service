3584ae1af185ad062b679dbe192c3c92f480e4a5
youngkyunIm <yee0230@naver.com>
2021-11-16 22:03:14 +0900

    StudentsRepository

---
+++ booking_service_01/src/main/java/com/example/booking_service_01/repository/StudentsRepository.java
@@ -0,0 +1,10 @@
+package com.example.booking_service_01.repository;
+
+import com.example.booking_service_01.entity.Students;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface StudentsRepository extends JpaRepository<Students, Integer>{
+    Students findBySid(Integer sid);
+}
