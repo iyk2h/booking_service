3960e658f6f5840f9346398b5ca8ad218ee29c86
youngkyunIm <yee0230@naver.com>
2021-11-16 22:01:31 +0900

    admin_chaged object type

--- booking_service_01/src/main/java/com/example/booking_service_01/service/AdminService.java
+++ booking_service_01/src/main/java/com/example/booking_service_01/service/AdminService.java
@@ -3,8 +3,6 @@ package com.example.booking_service_01.service;
 import com.example.booking_service_01.dto.AdminDTO;
 
 public interface AdminService {
-    
-    
     AdminDTO findByAno(Integer ano);
     boolean checkAno(Integer ano);
     Integer insertAdminDto(AdminDTO adminDTO);
