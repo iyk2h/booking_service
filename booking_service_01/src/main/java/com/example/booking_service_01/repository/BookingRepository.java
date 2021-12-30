305bd6da8b8f10ea5de96be16f8bc8a02f1438c4
youngkyunIm <yee0230@naver.com>
2021-12-30 19:00:45 +0900

    Get Booking List by facility

--- booking_service_01/src/main/java/com/example/booking_service_01/repository/BookingRepository.java
+++ booking_service_01/src/main/java/com/example/booking_service_01/repository/BookingRepository.java
@@ -3,6 +3,7 @@ package com.example.booking_service_01.repository;
 import java.util.List;
 
 import com.example.booking_service_01.entity.Booking;
+import com.example.booking_service_01.entity.Facility;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer>{
 //    List<Booking> findByBnoList(Integer bno);
+    
     Booking findByBno(Integer bno);
+    
+    List<Booking> findByFacility(Facility facility);
 }
