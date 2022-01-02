6a6a136a6ff0a08f654b5351174cedde4a95827b
youngkyunIm <yee0230@naver.com>
2022-01-02 18:17:29 +0900

    session login, logout

--- booking_service_01/src/main/java/com/example/booking_service_01/controller/AdminController.java
+++ booking_service_01/src/main/java/com/example/booking_service_01/controller/AdminController.java
@@ -2,8 +2,6 @@ package com.example.booking_service_01.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.http.HttpHeaders;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,13 +15,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
