59ea90c5dc9f6b9164ac9d701922e02a1cce70c2
youngkyunIm <yee0230@naver.com>
2022-02-01 16:21:07 +0900

    move facility manage control

--- booking_service_01/src/main/java/com/example/booking_service_01/controller/AdminController.java
+++ booking_service_01/src/main/java/com/example/booking_service_01/controller/AdminController.java
@@ -194,4 +194,71 @@ public class AdminController {
             return new ResponseEntity<>("삭제되었습니다.",HttpStatus.NO_CONTENT);
         }
     }
+    //----------- manage facility -------------//
+    //insert
+    @PostMapping(path = "/facility/join", produces = "application/json")
+    public ResponseEntity<?> insertFacility(@RequestBody FacilityDTO facilityDTO) {
+        if(facilityDTO!=null && facilityDTO.getFno()==null) {
+            return new ResponseEntity<>(facilityService.findByFno(facilityService.insertFacilityDto(facilityDTO)), HttpStatus.CREATED);
+        }
+        else{
+            return new ResponseEntity<>("잘못 입력되었습니다.",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //select all
+    @GetMapping(path="/facility", produces = "application/json")
+    public ResponseEntity<?> getAllFacility() {
+        return new ResponseEntity<>(facilityService.findAll(), HttpStatus.OK);
+    }
+
+    //Select by fno
+    @GetMapping(path="/facility/{fno}", produces = "application/json")
+    public ResponseEntity<?> getByFno(@PathVariable("fno") Integer fno) {
+        if(!facilityService.checkFno(fno)) {
+            return new ResponseEntity<>("fno can not found", HttpStatus.NOT_FOUND);
+        }
+        else {
+            FacilityDTO facilityDTO = facilityService.findByFno(fno);
+            return new ResponseEntity<>(facilityDTO, HttpStatus.OK);
+        }
+    }
+
+    //Update   
+    @PutMapping(path = "/facility/{fno}", produces = "application/json")
+    public ResponseEntity<?> updateFacility(@PathVariable("fno") Integer fno, @RequestBody FacilityDTO facilityDTO) {
+        FacilityDTO beforeDTO = facilityService.findByFno(fno);
+        if(facilityDTO != null){
+            Integer u_fno = facilityDTO.getFno()!=null?fno:beforeDTO.getFno();
+            String u_place = facilityDTO.getPlace()!=null?facilityDTO.getPlace():beforeDTO.getPlace();
+            String u_placeUrl = facilityDTO.getPlaceUrl()!=null?facilityDTO.getPlaceUrl():beforeDTO.getPlaceUrl();
+            Integer u_maxHour = facilityDTO.getMaxHour()!=null?facilityDTO.getMaxHour():beforeDTO.getMaxHour();
+            String u_name = facilityDTO.getName()!=null?facilityDTO.getName():beforeDTO.getName();
+
+            FacilityDTO updateDTO= FacilityDTO.builder()
+                .fno(u_fno)
+                .place(u_place)
+                .placeUrl(u_placeUrl)
+                .name(u_name)
+                .maxHour(u_maxHour)
+                .build();
+            
+            return new ResponseEntity<>(facilityService.findByFno(facilityService.update(updateDTO)), HttpStatus.CREATED);
+        }
+        else
+            return new ResponseEntity<>("Update fail", HttpStatus.BAD_REQUEST);
+    }
+
+    //Delete
+    @DeleteMapping(path="/facility/{fno}", produces = "application/json")
+    public ResponseEntity<?> deleteFacility(@PathVariable("fno") Integer fno) {
+        if(!facilityService.checkFno(fno))
+            return new ResponseEntity<>("Facility Number can not found", HttpStatus.NOT_FOUND);
+        else {
+            FacilityDTO facilityDTO = facilityService.findByFno(fno);
+            facilityService.delete(facilityDTO);
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        }
+    }
+
 }
